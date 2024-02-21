7ad352d37d201c074f0dfd7990bf80398bbc0bee
Abdul Kudus Musah <94498327+KudusBannah@users.noreply.github.com>
2024-02-21 10:24:18 +0000

    Add files via upload

---
+++ House robber II.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = {}
+
+        def dp(idx, flag):
+            if idx == len(nums) - 1:
+                return 0 if flag else nums[idx]
+
+            if idx == len(nums) - 2:
+                return nums[idx] if flag else max(nums[idx], nums[idx+1])
+
+            if (idx, flag) not in memo:
+                memo[(idx, flag)] = max(nums[idx] + dp(idx+2, flag), dp(idx+1, flag))
+            return memo[(idx, flag)]
+
+        if len(nums) < 3:
