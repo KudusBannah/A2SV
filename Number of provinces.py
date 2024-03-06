30bd228639d12676af0cc30dc01a180fcd528665
Abdul Kudus Musah <94498327+KudusBannah@users.noreply.github.com>
2024-03-06 11:42:18 +0000

    Add files via upload

--- Number of provinces.py
+++ Number of provinces.py
@@ -1,26 +1,45 @@
+class UnionFind:
+    def __init__(self, n):
+        self.my_dict = {}
+        self.provinces = n
+        self.size = {}
+        for i in range(n):
+            self.my_dict[i] = i
+            self.size[i] = 1
+
+    def find(self, curr):
+        if self.my_dict[curr] == curr:
+            return curr
+        self.my_dict[curr] = self.find(self.my_dict[curr])
+        return self.my_dict[curr]
+
+    def union(self, u, v):
+        l, r = self.find(u), self.find(v)
+        if l == r: 
+            return
+
+        self.provinces -= 1
+        if self.size[l] < self.size[r]:
+            self.my_dict[l] = r
+            self.size[r] += self.size[l]
+            return
+        self.my_dict[r] = l
+        self.size[l] += self.size[r]
+
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        def dfs(node):
-            nonlocal visited
-            if node in visited:
-                return
-            visited.add(node)
-            for connected_city in graph[node]:
-                dfs(connected_city)
+        union_find = UnionFind(len(isConnected))
 
-
-        graph = defaultdict(set)
-        n = len(isConnected)
-        for i in range(n):
-            for j in range(n):
+        for i in range(len(isConnected)):
+            for j in range(len(isConnected[i])):
                 if isConnected[i][j] == 1 and i != j:
-                    graph[i+1].add(j+1)
-                    graph[j+1].add(i+1)
+                    union_find.union(i, j)
+
+        return union_find.provinces
         
-        province_count = 0
-        visited = set()
-        for city in range(1, n+1):
-            if city not in visited:
-                dfs(city)
-                province_count += 1
-        return province_count
+        
+
+        
+
+        
