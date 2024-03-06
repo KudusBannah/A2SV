94f00e4fe81a0ca944df0a844fd1c0594b732627
Abdul Kudus Musah <94498327+KudusBannah@users.noreply.github.com>
2024-03-06 11:55:58 +0000

    Add files via upload

--- Find if path exists in graph.py
+++ Find if path exists in graph.py
@@ -1,17 +1,33 @@
+class UnionFind:
+    def __init__(self, n):
+        self.my_dict = {}
+        self.size = {}
+        for i in range(n):
+            self.my_dict[i] = i
+            self.size[i] = 1
+
+    def find(self, curr):
+        if curr == self.my_dict[curr]:
+            return curr
+        self.my_dict[curr] = self.find(self.my_dict[curr])
+        return self.my_dict[curr]
+
+    def union(self, u, v):
+        l, r = self.find(u), self.find(v)
+        if l == r:
+            return
+        if self.size[l] < self.size[r]:
+            self.my_dict[l] = self.my_dict[r]
+            self.size[l] += self.size[r]
+            return
+        self.my_dict[r] = self.my_dict[l]
+        self.size[r] += self.size[l]
+        
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        a_list = defaultdict(list)
-        for edge in edges:
-            a_list[edge[0]].append(edge[1])
-            a_list[edge[1]].append(edge[0])
+        union_find = UnionFind(n)
+        for u, v in edges:
+            union_find.union(u, v)
 
-        print(a_list)
-        def dfs(node, visited):
-            if node == destination:
-                return True
-            
-            visited.add(node)
-            for neighbour in a_list[node]:
-                if neighbour not in visited:
-                    if dfs(neighbour, visited): return True
-            return False
+        return union_find.find(source) == union_find.find(destination)
